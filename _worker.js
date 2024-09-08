d7314e6a043afb79348b1390b50086c348bf3fde
xiabaosen <marsggg@users.noreply.github.com>
2024-09-08 10:01:17 +0800

    修改uuid

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '2c1568e4-d103-472d-8361-1d8f8feb9a04';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
